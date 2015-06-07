ebe958154f339ea829df132e953fb6b565970600
Rafael Minguet García <rminguet@gmail.com>
2015-06-07 19:39:49 +0200

    Prueba contra AWS

--- server.js
+++ server.js
@@ -6,57 +6,37 @@
 var AWS = require('aws-sdk'),
   awsRegion = 'us-west-2',
   sqs = {},
+  queueUri = 'https://sqs.us-west-2.amazonaws.com/921644418190/sample';
   Hapi = require('hapi'),
   server = new Hapi.Server();
 
 //server.connection({ host: 'localhost', port: '3000' });
 server.connection({ port: '8081' });
 
-/* **** FUNCIONES SQS ********************** */
-function sendSqsMessage(sender) {
-  'use strict';
-
-  AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: awsRegion
-  });
-  sqs = new AWS.SQS();
-
-  var params = {
-    MessageBody: 'Aqui va el cuerpo del mensaje',
-    QueueUrl: 'https://sqs.us-west-2.amazonaws.com/921644418190/sample',
-    DelaySeconds: 0
-  };
-
-  sqs.sendMessage(params, function (err, data) {
-    if (err) {
-      console.log(err, err.stack);
-    } else {
-      console.log('YAY!!, mensaje enviado por ' + sender + '!');
-    }
-  });
-}
-/* **** END: FUNCIONES SQS ***************** */
-
 /* **** RUTAS ****************************** */
 server.route({
-  method: 'GET',
-  path: '/',
+  method: 'POST',
+  path: '/hola',
   handler: function (request, response) {
-    response('HoooOOOooolaaaAAAaaa sueeeEEEeeelooOOOooo!\n');
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: awsRegion
+    });
+/*
+    sqs = new AWS.SQS();
+
+    server.log('response: ', request.payload.name); // payload= {"name":"XXXX"}
+    server.log(
+      'Starting receive message',
+      '...a 200 response should be received.'
+    );
+
+    response();
+    */
   }
 });
 
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: function (request, response) {
-    sendSqsMessage(encodeURIComponent(request.params.name));
-    response('Your message ' + encodeURIComponent(request.params.name) +
-        ' has been sent to queue!');
-  }
-});
 /* **** END: RUTAS ************************** */
 
 /* **** MONITORES *************************** */
@@ -66,23 +46,7 @@ var options = {
   reporters: [{
     reporter: require('good-console'),
     events: { log: '*', response: '*' }
-  }
-  /* mas reporters... */
-  /*, {
-    reporter: require('good-file'),
-    events: { ops: '*' },
-    config: './test/fixtures/awesome_log'
-  }, {
-    reporter: 'good-http',
-    events: { error: '*' },
-    config: {
-      endpoint: 'http://prod.logs:3000',
-      wreck: {
-        headers: { 'x-api-key' : 12345 }
-      }
-    }
-  }*/
-  ]
+  }]
 };
 
 // registro los eventos a los que escucho
@@ -91,9 +55,8 @@ server.register({ register: require('good'), options: options }, function (err) 
     console.error(err); // error al tratar de cargar el plugin Good
   } else {
     server.start(function () {
-      server.log('info', 'Servidor escuchando en: ' + server.info.uri);
+      server.log('info', 'Servidor worker escuchando en: ' + server.info.uri);
     });
   }
-
 });
 /* **** END: MONITORES *********************** */
